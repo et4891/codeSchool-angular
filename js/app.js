885addfb8c708f988e1cbf98674ea2b0db9d836e
ET-Asus <et1103@hotmail.com>
2016-03-20 07:32:26 -0700

    Done section 1 stopped at section 2.3

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ET on 2016-03-19.
+ */
+
+//    Creating an angular module with application name "store" and the second parameter of [] is for dependencies usage which is empty now
+var app = angular.module("store", []);
