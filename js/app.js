ee5ada29193cf98e61d4ed7a1191782c73615db0
ET <et1103@hotmail.com>
2016-03-21 22:25:22 -0700

    Done all 5 sections. Lastly, used  to call json which includes the store-products which was in app.js stored in gems variable

--- js/app.js
+++ js/app.js
@@ -1,6 +1,55 @@
 /**
- * Created by ET on 2016-03-19.
+ * Created by ET on 2016-03-20.
  */
+(function(){
+     //Create a Module named gemStore so we can get started on this marketing journey
+    //Attach the gemStore module to our HTML page with a Directive
+    var app = angular.module("gemStore", ['store-directives']);
 
-//    Creating an angular module with application name "store" and the second parameter of [] is for dependencies usage which is empty now
-var app = angular.module("store", []);
+    //Storing data inside the controller
+    app.controller('StoreController', ['$http', function($http){
+        var store = this;
+        store.products = [];
+        $http.get('store-products.json').success(function(data){
+            store.products = data;
+        });
+    }]);
+
+    //Controller for the tab panel //removed and inserted into directives for refactoring purpose
+    // app.controller("TabController", function(){
+    //    this.tab = 1;
+    //    this.setTab = function(setTab){
+    //        this.tab = setTab;
+    //    };
+    //    this.isSet = function(isSet){
+    //        return this.tab === isSet;
+    //    };
+    // });
+
+    //Controller for Gallery  //removed and inserted into directives for refactoring purpose
+    // app.controller("GalleryController", function(){
+    //    this.current = 0;
+    //    this.setCurrent = function(setCurrent){
+    //        if(setCurrent === null){
+    //            this.current = 0;
+    //        }else{
+    //            this.current = setCurrent;
+    //        }
+    //    };
+    // });
+
+    //Review Form Controller
+    app.controller("ReviewController", function(){
+       // initial review as empty object
+       this.review = {};
+       this.addReview = function(product){
+           this.review.createdOn = Date.now();
+           product.reviews.push(this.review);
+           this.review = {};
+       };
+    });
+    
+    
+
+
+})();
